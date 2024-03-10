9ba0b2cbaf8aca42354c08a9a9b97554ca3c2329
Olatunji Peter <polalekan526@gmail.com>
2024-03-10 23:40:55 +0100

    New update added to Transaction Page

--- src/component/DashComp/TransHistory.jsx
+++ src/component/DashComp/TransHistory.jsx
@@ -4,6 +4,16 @@ import api from "../api";
 const TransHistory = () => {
     const [transHis, setTransHis] = useState('')
 
+    const option = {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      timeZone: "UTC", // Adjust the time zone as needed
+    };
+
     useEffect(() => {
         const fetchData = async () => {
           try {
@@ -24,10 +34,10 @@ const TransHistory = () => {
         <>
         <div className="flex justify-center">
           <div className="shadow-md justify-center w-[400px] px-4">
-        <h2 className="text-center my-4">This is your transaction History</h2>
+        <h2 className="text-center my-4 font-bold text-[19px]"> Transaction History</h2>
         
             {transHis && transHis.map(transaction => 
-                <div className="text-sm my-2">
+                <div className="text-sm my-3">
                 
                     <p>
                       <div className="flex w-full">
@@ -35,8 +45,8 @@ const TransHistory = () => {
                       <h2 className="ml-auto font-bold">-₦{transaction.amount}.00</h2>
                       </div>
                       <div className="flex">
-                      <h2> {transaction.date} . {transaction.recipient.firstname} {transaction.recipient.lastname} </h2>
-                        <h2 className="text-private ml-auto">{transaction.status}</h2>
+                      <h2 className="text-black text-opacity-60 text-[13px]"> {new Date(transaction.date).toLocaleString("en-US", option)}  {transaction.recipient.firstname} {transaction.recipient.lastname} </h2>
+                        <h2 className="text-private ml-auto font-bold text-[13px]">{transaction.status}</h2>
                       </div>
                     </p>
                 </div>
