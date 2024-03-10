9d50a7af236c31a6c464aada838d331fd2478efd
Olatunji Peter <polalekan526@gmail.com>
2024-03-10 23:18:25 +0100

    New interface added to transaction history page

--- src/component/DashComp/TransHistory.jsx
+++ src/component/DashComp/TransHistory.jsx
@@ -22,14 +22,27 @@ const TransHistory = () => {
 
     return ( 
         <>
-        <h2>This is your transaction History</h2>
+        <div className="flex justify-center">
+          <div className="shadow-md justify-center w-[400px] px-4">
+        <h2 className="text-center my-4">This is your transaction History</h2>
+        
             {transHis && transHis.map(transaction => 
-                <div >
+                <div className="text-sm my-2">
+                
                     <p>
-                        {transaction.sender.firstname} {transaction.sender.lastname}  sent ₦{transaction.amount} to {transaction.recipient.firstname} {transaction.recipient.lastname} 
+                      <div className="flex w-full">
+                      <h2 className="font-bold text-xs">Money Sent</h2>
+                      <h2 className="ml-auto font-bold">-₦{transaction.amount}.00</h2>
+                      </div>
+                      <div className="flex">
+                      <h2> {transaction.date} . {transaction.recipient.firstname} {transaction.recipient.lastname} </h2>
+                        <h2 className="text-private ml-auto">{transaction.status}</h2>
+                      </div>
                     </p>
                 </div>
             )}
+            </div>
+            </div>
         </>
      );
 }
